61ce5e413bd110667a55d3e52e071a7f5657097d
Abhishek kumar <ak4279673@gmail.com>
2025-05-08 20:07:08 +0530

    Model Trainer and mlflow commited

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ## Network Security project For Phishing Data
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.utils.main_utils.utils import read_yaml_file,write_yaml_file
 import sys
@@ -28,6 +29,13 @@ if __name__=="__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("Data Transformation completed")
+
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        logging.info("model trainer started")
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model trainer artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
